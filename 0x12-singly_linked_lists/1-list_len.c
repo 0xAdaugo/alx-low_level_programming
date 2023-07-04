4b13152fd25d3bc8e11275d81c44ba978b122215
0xAdaugo <Onuecheghekwuadaugo@gmail.com>
2023-07-03 18:59:37 -0700

    fixed 1-list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of elements in a linked list_t list.
+ * list_len - Returns the number of elements in a linked lists
+ * @h: Pointer to the head of the list.
+ * Return: Number of elements in a linked list.
  */
 size_t list_len(const list_t *h)
 {
-	size_t num = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		num++;
+		n++;
 		h = h->next;
 	}
 
-	return (num);
+	return (n);
 }
