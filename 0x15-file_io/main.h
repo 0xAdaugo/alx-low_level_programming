53db0695221a33513736c8f85253599272a3fc20
0xAdaugo <Onuecheghekwuadaugo@gmail.com>
2023-07-10 19:51:32 -0700

    README.md

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int append_text_to_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endif
